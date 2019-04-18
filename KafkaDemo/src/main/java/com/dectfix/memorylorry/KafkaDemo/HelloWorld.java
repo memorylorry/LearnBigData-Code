007f1b32fc69f4c3b1f9694935925cf1c158c833
memorylorry <294092869@qq.com>
2019-04-18 17:42:57 +0800

    first commit

---
+++ KafkaDemo/src/main/java/com/dectfix/memorylorry/KafkaDemo/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.dectfix.memorylorry.KafkaDemo;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Welcome to come KafkaDemo.");
+    }
+}
---
+++ KafkaDemo/src/main/java/com/dectfix/memorylorry/KafkaDemo/comsumer/SimpleConsumer.java
@@ -0,0 +1,29 @@
+package com.dectfix.memorylorry.KafkaDemo.comsumer;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class SimpleConsumer {
+    public static void main(String[] args) throws IOException {
+        //读取conf中的配置，创建输入流
+        InputStream conf = SimpleConsumer.class.getClass().getResourceAsStream("/simple-kafka.properties");
+        Properties props = new Properties();
+        props.load(conf);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Arrays.asList("test"));
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+        }
+    }
+
+}
